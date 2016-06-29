9330e9d4a06fa6e7d10133c3595cb626685c2a22
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-29 15:50:33 +0700

    ADD simple expressive api

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('opcache.enable', '0');
+
+use Zend\Expressive\AppFactory;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/test-param/{name}', function ($request, $response, $next) {
+    $response->getBody()->write('Hello, ' . $request->getAttribute('name'));
+    return $response;
+});
+
+$app->pipeRoutingMiddleware();
+$app->pipeDispatchMiddleware();
+
+$app->run();
